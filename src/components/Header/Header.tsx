df99f99bdff690f8950018b0b79f5392983b151f
Rahul Parihar <rahul.parihar@jalantechnologies.com>
2023-03-12 13:03:34 +0530

    mimage->mLogo namechange

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
-import mimage from "../../../memory.png";
+import mLogo from "../../../memory.png";
 import "./Header.css";
 function Header() {
   return (
     <div className="header">
-      <img src={mimage} />
+      <img src={mLogo} />
       <h2>Memory Game</h2>
     </div>
   );
