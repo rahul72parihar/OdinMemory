08dd6599ce0d914e1c8a048fb47467ec017c1d49
Rahul Parihar <rahul.parihar@jalantechnologies.com>
2023-03-12 12:28:56 +0530

    improved readability of functions

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -3,32 +3,18 @@ import { imageObjArray } from "../../assets";
 import "./Game.css";
 
 function Game() {
-  const [imageArray, setImageArray] = React.useState(imageObjArray);
-  const [highScore, setHighScore] = React.useState(0);
+  // STATES ALPHABETICALLY SORTED
   const [currScore, setCurrScore] = React.useState(0);
+  const [highScore, setHighScore] = React.useState(0);
+  const [imageArray, setImageArray] = React.useState(imageObjArray);
   const [visited, setVisited] = React.useState(new Set());
-
+  //Click on Image
   const handleClick = (name: string) => {
     shuffleArray();
     if (visited.has(name)) found();
     else notFound(name);
   };
-
-  const found = () => {
-    setCurrScore(0);
-    setVisited(new Set());
-  };
-
-  const notFound = (name: string) => {
-    setCurrScore((prev) => prev + 1);
-    setHighScore(Math.max(currScore + 1, highScore));
-    setVisited((prevSet) => {
-      const newSet = prevSet;
-      newSet.add(name);
-      return newSet;
-    });
-  };
-
+  // SHUFFLE ARRAY AFTER CLICK ON CARD
   const shuffleArray = () => {
     setImageArray(
       (
@@ -42,23 +28,51 @@ function Game() {
       }
     );
   };
+  // IF CARD FOUND ON SET
+  const found = () => {
+    resetScore();
+  };
+  const resetScore = () => {
+    setCurrScore(0);
+    setVisited(new Set());
+  };
+  // IF NOT FOUND IN VISITED SET
+  const notFound = (name: string) => {
+    updateScore();
+    addToVisitedSet(name);
+  };
+  const addToVisitedSet = (name: string) => {
+    setVisited((prevSet) => {
+      const newSet = prevSet;
+      newSet.add(name);
+      return newSet;
+    });
+  };
+  const updateScore = () => {
+    setCurrScore((prev) => prev + 1);
+    setHighScore(Math.max(currScore + 1, highScore));
+  };
+  // RENDER IMAGE
+  // Takes src and name as parameter
+  const renderImage = (name: string, src: string) => {
+    return (
+      <img
+        className="card"
+        key={name}
+        src={src}
+        alt={`image of ${name}`}
+        onClick={() => handleClick(name)}
+      />
+    );
+  };
   return (
     <>
-      <div className="scoreDiv">
+      <div className="score-div">
         <h2>Current Score : {currScore}</h2>
         <h2>High Score : {highScore}</h2>
       </div>
-      <div className="game">
-        {imageArray.map((curr) => {
-          return (
-            <img
-              key={curr.name}
-              src={curr.src}
-              alt={`image of ${curr.name}`}
-              onClick={() => handleClick(curr.name)}
-            />
-          );
-        })}
+      <div className="cards-div">
+        {imageArray.map((curr) => renderImage(curr.name, curr.src))}
       </div>
     </>
   );
